8697c42816805fc57038d01fa5c17dbfc1085d94
belloLucas <lucasbellodev@gmail.com>
2025-03-01 18:39:55 -0300

    Boilerplate criado

---
+++ README.md
@@ -0,0 +1,37 @@
+Esse é um boilerplate para um backend feito em NestJS utilizando Typescript e TypeORM.
+
+Esse boilerplate já contém todas as configurações necessárias para que haja conexão com o banco de dados (PostgreSQL, mas pode ser facilmente para outros bancos relacionais).
+
+## Principais comandos:
+
+#### Rodar o projeto
+
+```bash
+  npm run start:dev
+```
+
+##### Criação de resources (módulos)
+
+```bash
+  nest g resource nomeDaResource
+```
+
+#### Criação de migrations
+
+```bash
+  npm run migration:create -name=nomeMigration
+```
+
+#### Rodar migrations
+
+```bash
+  npm run migration:run
+```
+
+#### Reverter migrations
+
+```bash
+  npm run migration:revert
+```
+
+Importante lembrar de sempre ligar a extensão do ESLINT e do Prettier, para que o lint funcione no projeto todo.
---
+++ src/db/db.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigService, ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '../../.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + './entities/**'],
+        migrations: [__dirname + '/migrations/*.ts'],
+        synchronize: false,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DbModule {}
